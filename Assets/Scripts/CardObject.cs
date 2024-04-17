9cae935383b2a98e52b229f6df308bc92bb3129a
JA-FSU <78230541+JA-FSU@users.noreply.github.com>
2024-04-17 11:02:15 -0400

    Updated Cards - Suits
    Added Suits to each creature card.

--- Assets/Scripts/CardObject.cs
+++ Assets/Scripts/CardObject.cs
@@ -3,20 +3,37 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [System.Serializable]
-//public class Suits
-//{
+public enum Suits
+{
+    Clubs,
+    Diamonds,
+    Hearts,
+    Spades
+}
 
-//}
+public enum Hand
+{
+    Pair,
+    Straight,
+    FullHouse
+}
+
+public class HandPower
+{
+    public Hand Hand;
+    public string HandDescription;
+}
 
 [CreateAssetMenu(fileName="CardObject", menuName = "Create Card/Creature Card")]
 public class CardObject : ScriptableObject
 {
     public string Name;
     //public Image CreatureArt
-    //public Suits Suit;
+    public Suits Suit;
     public int Bet;
     public int Rank;
     public int Power;
     public string Description;
     public string CreatureType;
+    public HandPower[] handPowers;
 }
